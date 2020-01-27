8dbeb772ae0dc9bbc23a8f568301a6516a042634
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-01-27 09:40:53 +0100

    Add project files.

---
+++ SistemaDati/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace SistemaDati
+{
+    class Program
+    {
+        private static string line;
+        static void Main(string[] args)
+        {
+            const string FILEREAD = "file.txt";
+            const string FILEWRITE = "newfile.txt";
+            HashSet<int> intList = new HashSet<int>();
+            using (StreamReader r = new StreamReader(FILEREAD))
+            {
+                while ((line = r.ReadLine()) != null)
+                {
+                    int d = int.Parse(line);
+                    intList.Add(d);
+                }
+            }
+            using (StreamWriter w = new StreamWriter(FILEWRITE))
+            {
+                foreach(int s in intList)
+                {
+                    w.WriteLine(s);
+                }
+            }
+        }
+    }
+}
